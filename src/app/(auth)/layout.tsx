00006cf60eacfc55b544d775bc014e6f5bded0f7
Muh-Zaan <muhzaan14@gmail.com>
2024-06-18 20:47:13 +0700

    update: layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -8,16 +8,19 @@ type Props = {
 
 const AuthLayout = ({ children }: Props) => {
   return (
-    <div className="w-screen h-screen px-40 py-28 bg-[#eaeaea]">
+    <div className="w-screen h-screen px-28 py-20 bg-[#f1f1f1]">
       <div className="w-full h-full flex bg-white rounded-xl overflow-hidden shadow-xl">
-        <div className="w-2/4 p-20">{children}</div>
+        <div className="w-2/4 py-20 px-32">{children}</div>
         <div className="w-2/4 relative">
           <Image
             src={AuthImage}
-            fill
             alt="auth-image..."
             sizes="1000px"
+            fill
             priority
+            style={{
+              objectFit: "cover",
+            }}
           />
         </div>
       </div>
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="w-full py-">
+      <h1 className="text-3xl font-bold">Welcome back user!</h1>
+      <p className="text-sm mt-1 text-black/70">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, corrupti?
+        Rerum nulla est voluptate iure atque magni id ad
+      </p>
+    </div>
+  );
 };
 
 export default LoginPage;
